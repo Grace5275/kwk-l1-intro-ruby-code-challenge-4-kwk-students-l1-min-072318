d5d5b7d2b799dcdf3678083cf66f8ac65a0efcac
Grace5275 <grace_madson@outlook.com>
2018-07-25 21:15:43 +0000

    automatically backed up by learn

--- least_coins.rb
+++ least_coins.rb
@@ -1,37 +1,37 @@
-#write out your code here
+# #write out your code here
 
 
-def least_coins(cents)
-  coins = {:quarter => 0, :dimes => 0, :nickles => 0, :pennies => 0} 
-  quarters = cents / 25
-  if quarters == 0 
-    coins[:quarters]= 0 
-  elsif quarters != 0 
-    coins[:quarters] = quarters
-    cents = cents - quarters * 25
-  end 
+# def least_coins(cents)
+#   coins = {:quarter => 0, :dimes => 0, :nickles => 0, :pennies => 0} 
+#   quarters = cents / 25
+#   if quarters == 0 
+#     coins[:quarters]= 0 
+#   elsif quarters != 0 
+#     coins[:quarters] = quarters
+#     cents = cents - quarters * 25
+#   end 
   
-  dimes = cents / 10 
-  if dimes == 0 
-    coins[:dimes]= 0 
-  elsif dimes != 0 
-    coins[:dimes] = dimes 
-    cents = cents - dimes * 10 
-  end 
+#   dimes = cents / 10 
+#   if dimes == 0 
+#     coins[:dimes]= 0 
+#   elsif dimes != 0 
+#     coins[:dimes] = dimes 
+#     cents = cents - dimes * 10 
+#   end 
   
-  nickles = cents / 5 
-  if nickles == 0 
-    coins[:nickles]= 0 
-  elsif nickles != 0 
-    coins[:nickles] = nickles 
-    cents = cents - nickles * 5 
-  end 
+#   nickles = cents / 5 
+#   if nickles == 0 
+#     coins[:nickles]= 0 
+#   elsif nickles != 0 
+#     coins[:nickles] = nickles 
+#     cents = cents - nickles * 5 
+#   end 
   
-  coins[:pennies]= cents
+#   coins[:pennies]= cents
   
-  print coins 
-#Code your answer here!
-end
+#   print coins 
+# #Code your answer here!
+# end
 
 
-least_coins(29)
+# least_coins(29)
